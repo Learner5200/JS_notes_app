5b2be0e44856aee424f2190702070a2f0540921e
Chris Cooksley <christopher.cooksley@gmail.com>
2018-11-15 17:47:12 +0000

    tests done

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -8,21 +8,27 @@ describe("NoteController", function() {
     });
   });
 
-  noteList.create("Favourite drink: seltzer")
-
   describe(".loadNotesHTML()", function() {
-    it("adds list to div on page", function() {
-      var htmlMock = {
-        innerHTML: "string"
-      };
-      document.getElementById = function() { return htmlMock; };
+    document.getElementByIdOriginal = document.getElementById;
+    var htmlMock = {
+      innerHTML: "string"
+    };
+    document.getElementById = function() { return htmlMock; };
+
+    it("adds empty list to div on page", function() {
       controller.loadNotesHTML();
-      var expression = (htmlMock.innerHTML == "<ul><li><a href='#notes/0'>Favourite drink: sel...</a></li></ul>");
-      expect(expression).toBe(true);
+      expect(htmlMock.innerHTML).toBe("<ul></ul>");
     });
+    it("displays items in list on page", function() {
+      noteList.create("Favourite drink: seltzer");
+      controller.loadNotesHTML();
+      expect(htmlMock.innerHTML).toBe("<ul><li><a href='#notes/0'>Favourite drink: sel...</a></li></ul>");
+    });
+    document.getElementById = document.getElementByIdOriginal
   });
 
   describe(".displayCurrentNote()", function() {
+    document.getElementByIdOriginal = document.getElementById;
     it("displays text of note on page", function() {
       var htmlMock = {
         innerHTML: "string"
@@ -30,9 +36,9 @@ describe("NoteController", function() {
       document.getElementById = function() { return htmlMock; };
       window.location.hash = "#notes/0";
       controller.displayCurrentNote();
-      var expression = (htmlMock.innerHTML == "<div>Favourite drink: seltzer</div>");
-      expect(expression).toBe(true);
+      expect(htmlMock.innerHTML).toBe("<div>Favourite drink: seltzer</div>");
     });
+    document.getElementById = document.getElementByIdOriginal
   });
 
   describe(".displayNoteOnClick()", function() {
@@ -49,4 +55,23 @@ describe("NoteController", function() {
       expect(eventListenerArguments).toBe(expectedArguments);
     });
   });
+
+  describe(".listenForSubmit()", function() {
+    it("updates html upon submit event", function() {
+      var div = document.createElement("div");
+      div.id = "text";
+      div[0] = {value: "TEST"};
+      document.body.appendChild(div);
+
+      var app = document.createElement("app");
+      app.id = "app";
+      document.body.appendChild(app);
+
+      controller.listenForSubmit();
+      var submit = new Event("submit");
+      div.dispatchEvent(submit);
+      expect(app.innerHTML).toBe('<ul><li><a href="#notes/0">Favourite drink: sel...</a></li><li><a href="#notes/1">TEST</a></li></ul>');
+    })
+  });
+
 });
